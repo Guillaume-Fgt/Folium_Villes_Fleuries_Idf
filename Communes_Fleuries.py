3058d2c50ac0b8e58fb02258ee7c51dcf70840d4
Guillaume_F <guillaume.faguet@protonmail.com>
2021-01-08 13:18:08 +0100

    add legend

--- Communes_Fleuries.py
+++ Communes_Fleuries.py
@@ -1,11 +1,13 @@
 import folium
+import json
+from branca.element import Template, MacroElement
 
-# charger data
-communes_geo_0 = "communes-ile-de-france_fleurs_0"
-communes_geo_1 = "communes-ile-de-france_fleurs_1"
-communes_geo_2 = "communes-ile-de-france_fleurs_2"
-communes_geo_3 = "communes-ile-de-france_fleurs_3"
-communes_geo_4 = "communes-ile-de-france_fleurs_4"
+# pour avoir le nbr par catégorie. à faire
+# with open(
+#     "communes-ile-de-france_fleurs_4",
+# ) as f:
+#     data_fleurs = json.load(f, encoding="utf-8")
+# print(len(data_fleurs["features"]))
 
 # création map centrée sur Paris
 m = folium.Map(
@@ -64,4 +66,103 @@ commune_4 = add_data_GeoJson(m, 4)
 
 
 folium.LayerControl().add_to(m)
+
+
+template = """
+{% macro html(this, kwargs) %}
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Draggable - Default functionality</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  
+  <script>
+  $( function() {
+    $( "#maplegend" ).draggable({
+                    start: function (event, ui) {
+                        $(this).css({
+                            right: "auto",
+                            top: "auto",
+                            bottom: "auto"
+                        });
+                    }
+                });
+});
+
+  </script>
+</head>
+<body>
+
+ 
+<div id='maplegend' class='maplegend' 
+    style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+     border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+     
+<div class='legend-title'>Nbr Fleurs Communes <br> IDF 2020 </div>
+<div class='legend-scale'>
+  <ul class='legend-labels'>
+    <li><span style='background:#f6eff7;opacity:0.7;'></span>0</li>
+    <li><span style='background:#bdc9e1;opacity:0.7;'></span>1</li>
+    <li><span style='background:#67a9cf;opacity:0.7;'></span>2</li>
+    <li><span style='background:#1c9099;opacity:0.7;'></span>3</li>
+    <li><span style='background:#016c59;opacity:0.7;'></span>4</li>
+
+  </ul>
+</div>
+</div>
+ 
+</body>
+</html>
+
+<style type='text/css'>
+  .maplegend .legend-title {
+    text-align: center;
+    margin-bottom: 5px;
+    font-weight: bold;
+    font-size: 90%;
+    }
+  .maplegend .legend-scale ul {
+    margin: 0;
+    margin-bottom: 5px;
+    padding: 0;
+    float: left;
+    list-style: none;
+    }
+  .maplegend .legend-scale ul li {
+    font-size: 80%;
+    list-style: none;
+    margin-left: 0;
+    line-height: 18px;
+    margin-bottom: 2px;
+    }
+  .maplegend ul.legend-labels li span {
+    display: block;
+    float: left;
+    height: 16px;
+    width: 30px;
+    margin-right: 5px;
+    margin-left: 0;
+    border: 1px solid #999;
+    }
+  .maplegend .legend-source {
+    font-size: 80%;
+    color: #777;
+    clear: both;
+    }
+  .maplegend a {
+    color: #777;
+    }
+</style>
+{% endmacro %}"""
+
+macro = MacroElement()
+macro._template = Template(template)
+
+m.get_root().add_child(macro)
 m.save("map.html")
