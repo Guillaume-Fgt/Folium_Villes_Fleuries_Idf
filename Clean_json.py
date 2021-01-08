53e8ee337691e9faf302f2104b467eea30602714
Guillaume_F <guillaume.faguet@protonmail.com>
2021-01-08 12:26:10 +0100

    modif pour avoir layers séparés

--- Clean_json.py
+++ Clean_json.py
@@ -5,30 +5,56 @@ with open(
     "communes-dile-de-france-labellisees-villes-et-villages-fleuris-2020 - Copy.json",
     "rb",
 ) as f:
-    data = json.load(f, encoding="utf-8")
+    data_fleurs = json.load(f, encoding="utf-8")
 
 with open(
-    "communes-ile-de-france - Copy.geojson", "rb"
+    "communes-ile-de-france.geojson", "rb"
 ) as f2:  # rb pour gérer les caractères à accents
-    data_2 = json.load(
+    data_commune = json.load(
         f2, encoding="utf-8"
     )  # utf8 en complément de rb pour les accents
 
+# on commence par créer une clé fleurs pour toutes les villes
+for feature_commune in data_commune["features"]:
+    feature_commune["properties"]["fleurs"] = 0
 
-for feature_2 in data_2["features"]:
-    feature_2["properties"]["fleurs"] = 0
 
-
-for feature_2 in data_2["features"]:
-    for feature in data["features"]:
+for feature_commune in data_commune["features"]:
+    for feature_fleurs in data_fleurs["features"]:
         if (
-            feature_2["properties"]["nom"]
-            == feature["properties"]["commune_officielle"]
+            feature_commune["properties"]["nom"]
+            == feature_fleurs["properties"]["commune_officielle"]
         ):
-            feature_2["properties"]["fleurs"] = feature["properties"]["fleurs"]
+            feature_commune["properties"]["fleurs"] = feature_fleurs["properties"][
+                "fleurs"
+            ]
+
+data_fleurie_0 = []
+data_fleurie_1 = []
+data_fleurie_2 = []
+data_fleurie_3 = []
+data_fleurie_4 = []
+
+for feature_commune in data_commune["features"]:
+    if feature_commune["properties"]["fleurs"] == 0:
+        data_fleurie_0.append(data_commune["features"].index(feature_commune))
+    elif feature_commune["properties"]["fleurs"] == 1:
+        data_fleurie_1.append(data_commune["features"].index(feature_commune))
+    elif feature_commune["properties"]["fleurs"] == 2:
+        data_fleurie_2.append(data_commune["features"].index(feature_commune))
+    elif feature_commune["properties"]["fleurs"] == 3:
+        data_fleurie_3.append(data_commune["features"].index(feature_commune))
+    elif feature_commune["properties"]["fleurs"] == 4:
+        data_fleurie_4.append(data_commune["features"].index(feature_commune))
+
+for index in sorted(
+    itertools.chain(data_fleurie_0, data_fleurie_1, data_fleurie_2, data_fleurie_3),
+    reverse=True,
+):
+    del data_commune["features"][index]
 
 
 with open(
-    "communes-ile-de-france - Copy2.geojson", "w"
+    "communes-ile-de-france_fleurs_4", "w"
 ) as f:  # rb pour gérer les caractères à accents
-    json.dump(data_2, f)
+    json.dump(data_commune, f)
--- Communes_Fleuries.py
+++ Communes_Fleuries.py
@@ -1,10 +1,13 @@
 import folium
-import branca
 
+# charger data
+communes_geo_0 = "communes-ile-de-france_fleurs_0"
+communes_geo_1 = "communes-ile-de-france_fleurs_1"
+communes_geo_2 = "communes-ile-de-france_fleurs_2"
+communes_geo_3 = "communes-ile-de-france_fleurs_3"
+communes_geo_4 = "communes-ile-de-france_fleurs_4"
 
-communes_geo = "communes-ile-de-france - Copy2.geojson"
-
-
+# création map centrée sur Paris
 m = folium.Map(
     location=[48.7, 2.3],
     zoom_start=9,
@@ -13,48 +16,52 @@ m = folium.Map(
 
 folium.TileLayer("CartoDB positron", name="Light Map", control=False).add_to(m)
 
-colorlist = ["#f6eff7", "#bdc9e1", "#67a9cf", "#1c9099", "#016c59"]
-colormap = branca.colormap.StepColormap(
-    colorlist,
-    index=[0, 1, 2, 3, 4],
-    vmin=0,
-    vmax=4,
-    caption="Nbr de fleurs de la commune",
-)
 
-# interactivity
-style_function = lambda x: {
-    # "fillColor": colormap[x["properties"]["fleurs"]],
-    "fillColor": colormap(
-        x["properties"]["fleurs"] + 0.1
-    ),  # +0.1 pour gérer l'index de colormap
-    "color": "#000000",
-    "fillOpacity": 0.5,
-    "weight": 0.1,
-}
+# define the color of the Geojon data (https://leafletjs.com/reference-1.6.0.html#path-option)
+colorlist = ["#f6eff7", "#bdc9e1", "#67a9cf", "#1c9099", "#016c59"]
+
+
+def style_function(number):
+    return lambda x: {
+        "color": colorlist[number],
+        "fillOpacity": 0.5,
+        "weight": 0.1,
+    }
+
+
 highlight_function = lambda x: {
     "fillColor": "#000000",
     "color": "#000000",
     "fillOpacity": 0.50,
     "weight": 0.3,
 }
-NIL = folium.features.GeoJson(
-    communes_geo,
-    style_function=style_function,
-    highlight_function=highlight_function,
-    tooltip=folium.features.GeoJsonTooltip(
-        fields=["nom", "code", "fleurs"],
-        aliases=["Commune: ", "Code Postal: ", "Nbr Fleurs: "],
-        style=(
-            "background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"
+
+
+def add_data_GeoJson(map, number):
+    commune_data = folium.features.GeoJson(
+        data="communes-ile-de-france_fleurs_" + str(number),
+        style_function=style_function(number),
+        highlight_function=highlight_function,
+        tooltip=folium.features.GeoJsonTooltip(
+            fields=["nom", "code", "fleurs"],
+            aliases=["Commune: ", "Code Postal: ", "Nbr Fleurs: "],
+            style=(
+                "background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;"
+            ),
+            sticky=False,
         ),
-        sticky=False,
-    ),
-    name="Communes fleuries 2020 IDF",
-)
-m.add_child(NIL)
-m.keep_in_front(NIL)
-#
-colormap.add_to(m)
+        name="Communes " + str(number) + " fleurs IDF",
+    )
+    map.add_child(commune_data)
+    map.keep_in_front(commune_data)
+
+
+commune_0 = add_data_GeoJson(m, 0)
+commune_1 = add_data_GeoJson(m, 1)
+commune_2 = add_data_GeoJson(m, 2)
+commune_3 = add_data_GeoJson(m, 3)
+commune_4 = add_data_GeoJson(m, 4)
+
+
 folium.LayerControl().add_to(m)
 m.save("map.html")
